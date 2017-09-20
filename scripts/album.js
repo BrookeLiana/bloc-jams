16f37a3596cfcaae4a175cf1d6e35d218a72a745
Brooke <b.lianahargrove@gmail.com>
2017-09-20 18:20:16 -0400

    Assignment 23 Completed

--- scripts/album.js
+++ scripts/album.js
@@ -1,32 +1,48 @@
 // Example Album
  var albumPicasso = {
-     title: 'The Colors',
+     name: 'The Colors',
      artist: 'Pablo Picasso',
      label: 'Cubism',
      year: '1881',
      albumArtUrl: 'assets/images/album_covers/01.png',
      songs: [
-         { title: 'Blue', duration: '4:26' },
-         { title: 'Green', duration: '3:14' },
-         { title: 'Red', duration: '5:01' },
-         { title: 'Pink', duration: '3:21'},
-         { title: 'Magenta', duration: '2:15'}
+         { name: 'Blue', duration: '4:26' },
+         { name: 'Green', duration: '3:14' },
+         { name: 'Red', duration: '5:01' },
+         { name: 'Pink', duration: '3:21'},
+         { name: 'Magenta', duration: '2:15'}
      ]
  };
 
  // Another Example Album
  var albumMarconi = {
-     title: 'The Telephone',
+     name: 'The Telephone',
      artist: 'Guglielmo Marconi',
      label: 'EM',
      year: '1909',
      albumArtUrl: 'assets/images/album_covers/20.png',
      songs: [
-         { title: 'Hello, Operator?', duration: '1:01' },
-         { title: 'Ring, ring, ring', duration: '5:01' },
-         { title: 'Fits in your pocket', duration: '3:21'},
-         { title: 'Can you hear me now?', duration: '3:14' },
-         { title: 'Wrong phone number', duration: '2:15'}
+         { name: 'Hello, Operator?', duration: '1:01' },
+         { name: 'Ring, ring, ring', duration: '5:01' },
+         { name: 'Fits in your pocket', duration: '3:21'},
+         { name: 'Can you hear me now?', duration: '3:14' },
+         { name: 'Wrong phone number', duration: '2:15'}
+     ]
+ };
+
+ // Another Example Album
+ var albumMadona = {
+     name: 'Sunshine',
+     artist: 'Fifth Harmnony',
+     label: 'PRG',
+     year: '2015',
+     albumArtUrl: 'assets/images/album_covers/02.png',
+     songs: [
+         { name: 'Hello, Operator?', duration: '1:01' },
+         { name: 'Ring, ring, ring', duration: '5:01' },
+         { name: 'Fits in your pocket', duration: '3:21'},
+         { name: 'Can you hear me now?', duration: '3:14' },
+         { name: 'Wrong phone number', duration: '2:15'}
      ]
  };
 
@@ -41,30 +57,38 @@
 
     return template;
 };
+    var albumTitle = document.getElementsByClassName('album-view-title')[0];
+    var albumArtist = document.getElementsByClassName('album-view-artist')[0];
+    var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
+    var albumImage = document.getElementsByClassName('album-cover-art')[0];
+    var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
 
 var setCurrentAlbum = function(album) {
-     // #1
-     var albumTitle = document.getElementsByClassName('album-view-title')[0];
-     var albumArtist = document.getElementsByClassName('album-view-artist')[0];
-     var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
-     var albumImage = document.getElementsByClassName('album-cover-art')[0];
-     var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+    // #2
+    albumTitle.firstChild.nodeValue = album.title;
+    albumArtist.firstChild.nodeValue = album.artist;
+    albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
+    albumImage.setAttribute('src', album.albumArtUrl);
 
-     // #2
-     albumTitle.firstChild.nodeValue = album.title;
-     albumArtist.firstChild.nodeValue = album.artist;
-     albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
-     albumImage.setAttribute('src', album.albumArtUrl);
+    // #3
+    albumSongList.innerHTML = '';
 
-     // #3
-     albumSongList.innerHTML = '';
+    // #4
+    for (var i = 0; i < album.songs.length; i++) {
+        albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
+    }
+};
 
-     // #4
-     for (var i = 0; i < album.songs.length; i++) {
-         albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
-     }
- };
+window.onload = function() {
+    setCurrentAlbum(albumPicasso);
 
- window.onload = function() {
-     setCurrentAlbum(albumPicasso);
- };
+    var albums = [albumPicasso, albumMarconi, albumMadona];
+    var index = 1;
+    albumImage.addEventListener("click", function(event){
+      setCurrentAlbum(albums[index]);
+      index++;
+      if (index == albums.length){
+        index = 0;
+      }
+    });
+};
