2752f7f782c6d033d94a5bf27a3572ed3b37717d
Brooke <b.lianahargrove@gmail.com>
2017-09-24 13:44:07 -0400

    Fix console errors

--- scripts/landing.js
+++ scripts/landing.js
@@ -3,42 +3,28 @@ var animatePoints = function(){
 
   var revealPoint = function(){
           // #7
-          $(this).css({
-              opacity: 1,
-              transform: 'scaleX(1) translateY(0)'
-          });
-          var revealPoint = function() {
-        // #7
-        $(this).css({
-            opacity: 1,
-            transform: 'scaleX(1) translateY(0)'
-        });
+    $(this).css({
+        opacity: 1,
+        transform: 'scaleX(1) translateY(0)'
+    });
+  };
 
-    };
+ $.each($('.point'), revealPoint);
+};
 
- var animatePoints = function(points) {
-      forEach(points, revealPoint);
-    };
+$(window).load(function() {
+  // #1
+   if ($(window).height() > 950) {
+       animatePoints();
+   }
+   // #2
+   var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
 
-    $(window).load(function() {
-      // #1
-           if ($(window).height() > 950) {
-               animatePoints();
-           }
-           // #2
-                var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
-
-                // #3
-     $(window).scroll(function(event) {
-                  // #4
-       if ($(window).scrollTop() >= scrollDistance) {
-             animatePoints();
-
-           });
-
-         window.addEventListener("scroll", function(event) {
-           if (pointsArray[0].getBoundingClientRect().top <= 500) {
-             animatePoints(pointsArray);
-           }
-         });
- }
+            // #3
+   $(window).scroll(function(event) {
+              // #4
+     if ($(window).scrollTop() >= scrollDistance) {
+         animatePoints();
+     }
+   });
+});
